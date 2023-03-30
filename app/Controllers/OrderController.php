23c174c55209d3a80c21c6bd62458e3dc277dd3a
Áron Széchenyi <aronszechenyi@gmail.com>
2023-03-30 16:11:38 +0200

    demo app redesign

--- app/Controllers/OrderController.php
+++ app/Controllers/OrderController.php
@@ -28,37 +28,36 @@ class OrderController extends BaseController
         $this->render('menu', ["cards" => $cards]);
     }
 
-    /*
-     * no clue how to implement cycles in the templating lib
-     */
     private function assembleCards($value): string
     {
-        return ' <table class="container"><tr>
-            <div id="' . $value["ID"] . '"> 
-                   <td class="pizzaneve"> <h2>' . $value["NAME"] . ' </h2></td>
-                   <td class="descript"> <p> ' . $value["DESCRIPTION"] . ' </p></td>
-                   <td class="pizza">
-                        <img id="kep" src="' . $value["PICTURE"] . '">
-                        </td>
-                 <td class="price"> <h2>' . $value["PRICE"] . ' </h2></td>
-                 <td class="kosár">
-                 <form action="pizzak.php" method="get">
-                 <input type="hidden" id="id" name="id" value="' . $value["ID"] . '">
-                 <input type="hidden" id="name" name="name" value="' . $value["NAME"] . '">
-                 <button type="Submit" value="submit">
-                     <img id="kep2" src="cart.png"/>
-                 </button>
-             </form>
-                 </td>
-               </div>
-               </tr></table><br>';
+        return '<div class="col-md-4">
+            <div class="card"">
+                <img class="card-img-top" src="img/' . $value["PICTURE"] . '">
+                <div class="card-body">
+                    <h5 class="card-title">' . $value["NAME"] . '</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">' . $value["PRICE"] . ' HUF</h6>
+                    <p class="card-text">' . $value["DESCRIPTION"] . '</p>
+                    <form action="/menu" method="POST">
+                         <input type="hidden" id="id" name="id" value="' . $value["ID"] . '">
+                         <input type="hidden" id="name" name="name" value="' . $value["NAME"] . '">
+                         <button class="card-button" type="Submit" value="submit">
+                             Buy!
+                         </button>
+                    </form>
+                </div>
+            </div>
+        </div>';
     }
 
     #[Route('/menu', 'POST')]
     public function postTest(array $params): void
     {
-        print "<h1>POST EXECUTED</h1>";
-        print_r($params);
-        $this->render('index');
+        $pizza = new ReflectionClass(Pizza::class);
+        $pizzas = $pizza->newInstanceWithoutConstructor()->getAllPizza();
+        $cards = "";
+        foreach ($pizzas as $key => $value) {
+            $cards .= $this->assembleCards($value);
+        }
+        $this->render('menu', ["cards" => $cards]);
     }
 }
--- util/Globals.php
+++ util/Globals.php
@@ -6,7 +6,7 @@ namespace Util;
 
 class Globals
 {
-    public static bool $DEBUG = true;
+    public static bool $DEBUG = false;
     public static string $DB_NAME = "a3kwh7";
     public static string $DB_USERNAME = "root";
     public static string $DB_PASSWORD = "";
